df69054b564b854ffe17919da3b389e715ea3c69
Tristant2005 <tristanthehuman9@gmail.com>
2026-03-21 17:40:54 -0700

    monograph code

--- backend/medical_db.py
+++ backend/medical_db.py
@@ -1,6 +1,6 @@
 from backend.search_med import search
-#from summarize_monograph import summarize_monograph
-#from search_monograph import din_to_monograph
+from backend.summarize_monograph import summarize_monograph
+from backend.search_monograph import din_to_monograph
 
 
 def final_search_din(query: str):
@@ -14,22 +14,28 @@ def final_search_din(query: str):
     return results
     
     
-# def final_monograph(results: dict):
-#     if len(results["DIN"]) == 1:
-#         din = results["DIN"][0]
-#         din = din.zfill(8)
-#         print(din)
+def final_monograph(results: dict):
+    if len(results["DIN"]) == 1:
+        din = results["DIN"][0]
+        # din = din.zfill(8)
+        print(din)
         
-#         monograph = din_to_monograph(din)
-#         summary = summarize_monograph(monograph["pdf_url"])
-#         # print(summary)
-#         print(f"PDF size: {len(monograph['pdf_b64'])} chars (base64)")
+        try:
+            monograph = din_to_monograph(din)
+        except ValueError:
+            return "No monograph PDF found for DIN '00000019'"
+            
+        summary = summarize_monograph(monograph["pdf_url"])
+        print(f"PDF size: {len(monograph['pdf_b64'])} chars (base64)")
         
-#         return summary
+        return summary
         
 
+query = "02549883"
+results = final_search_din(query)
+print(results)
 
-# monograph = final_monograph(results)
-# print(monograph)
+monograph = final_monograph(results)
+print(monograph)
 
 
